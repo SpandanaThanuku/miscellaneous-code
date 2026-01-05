4794f4b907e95aee2d086d9d635189edddc2b212
Spandana <spandanathanuku3@gmail.com>
2026-01-05 12:03:22 +0530

    terraform code to create prometheus server and pipeline (goCD) to run the code

--- Prometheus/main.tf
+++ Prometheus/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 data "aws_ami" "centos8" {
   most_recent = true
-  name_regex = "Centos-8-Devops-Practice"
+  name_regex  = "Centos-8-DevOps-Practice"
   owners = ["973714476881"]
 }
 
