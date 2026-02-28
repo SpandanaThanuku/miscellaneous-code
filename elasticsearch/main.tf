fd746d574d13660bbed39d22cbc99d387e7b0b4c
Spandana <spandanathanuku3@gmail.com>
2026-02-28 18:14:17 +0530

    created dns record for logstash

--- elasticsearch/main.tf
+++ elasticsearch/main.tf
@@ -39,3 +39,11 @@ resource "aws_route53_record" "elasticsearch" {
   records = [aws_instance.elasticsearch.public_ip]
 }
 
+resource "aws_route53_record" "logstash" {
+  zone_id = "Z10004612WLGZAG3UBPGB"
+  name    = "logstash"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.elasticsearch.private_ip]
+}
+
