d871fca464697e1ab8fd05500bcf29ba7cdb553b
Spandana <spandanathanuku3@gmail.com>
2026-03-27 23:09:53 +0530

    changed the password of elastic search and updated parameter file

--- elasticsearch/ansible.sh
+++ elasticsearch/ansible.sh
@@ -1,4 +1,4 @@
 ssh_username=$(aws ssm get-parameter --name="ssh.username" --with-decryption --query 'Parameter.Value' --output text)
 ssh_password=$(aws ssm get-parameter --name="ssh.password" --with-decryption --query 'Parameter.Value' --output text)
 
-ansible-playbook -i 54.163.94.254, elasticsearch.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
+ansible-playbook -i 44.204.174.229, elasticsearch.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
