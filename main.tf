eec8dcbd26380512e3772d1de02736bf2a4b4b78
Spandana <spandanathanuku3@gmail.com>
2026-04-07 11:15:49 +0530

    created instance for sonarqube

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "ec2" {
   source = "./modules/ec2"
   for_each        = var.tools
   tool            = each.key
-  instance_type   = each.value["instance_type"]
+  instance_type   = each.value["instance"]
 }
