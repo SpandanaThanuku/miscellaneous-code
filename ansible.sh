c2b439827c6ea9f56819545e5fae108b27f0a3ec
Spandana <spandanathanuku3@gmail.com>
2026-04-07 17:08:46 +0530

    extracting the IP of sonarqube

---
+++ ansible.sh
@@ -0,0 +1,7 @@
+ssh_username=$(aws ssm get-parameter --name="ssh.username" --with-decryption --query 'Parameter.Value' --output text)
+ssh_password=$(aws ssm get-parameter --name="ssh.password" --with-decryption --query 'Parameter.Value' --output text)
+
+tool_name=$1
+IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=sonarqube --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+ansible-playbook -i ${IP}, sonarqube.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
+
