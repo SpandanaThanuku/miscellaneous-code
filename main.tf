9024584929c3f368ddb93c001512494c0368b7a0
Spandana <spandanathanuku3@gmail.com>
2026-04-10 22:22:08 +0530

    Adding Load balancer for tools

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@ module "ec2" {
   tool            = each.key
   instance_type   = each.value["instance"]
 }
+
+module "alb" {
+  source = "./modules/alb"
+}
+
---
+++ modules/alb/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {
+  default = "vpc-07dd983902cc0e9b5"
+}
+
+variable "acm_arn" {
+  default = "arn:aws:acm:us-east-1:348220191398:certificate/60d0f201-fc4c-426e-af49-bddf5f9a5c60"
+}
