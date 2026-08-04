fb41ecaeab9912338e06c582dec46cb30c8baa11
Spandana <spandanathanuku3@gmail.com>
2026-08-04 18:51:13 +0530

    creating jenkins pipeline

--- variables.tf
+++ variables.tf
@@ -2,47 +2,55 @@ variable "tools" {
   default = {
 
     sonarqube = {
-      instance    = "t3.large"
-      port        = 9000
-      priority    = 100
-      use_spot    = true
+      instance = "t3.large"
+      port     = 9000
+      priority = 100
+      use_spot = true
       policy_list = []
     }
     elasticsearch = {
-      instance    = "m6in.large"
-      port        = 80
-      priority    = 101
-      use_spot    = true
+      instance = "m6in.large"
+      port     = 80
+      priority = 101
+      use_spot = true
       policy_list = []
     }
     prometheus = {
-      instance    = "t3.small"
-      port        = 9090
-      priority    = 102
-      use_spot    = true
+      instance = "t3.small"
+      port     = 9090
+      priority = 102
+      use_spot = true
       policy_list = ["ec2:Describe*"]
     }
     grafana = {
-      instance    = "t3.small"
-      port        = 3000
-      priority    = 103
-      use_spot    = true
+      instance = "t3.small"
+      port     = 3000
+      priority = 103
+      use_spot = true
       policy_list = []
     }
     alertmanager = {
-      instance    = "t3.small"
-      port        = 9093
-      priority    = 104
-      use_spot    = true
+      instance = "t3.small"
+      port     = 9093
+      priority = 104
+      use_spot = true
       policy_list = []
     }
     artifactory = {
-     # instance = "r6i.xlarge"
-      instance    = "t3.medium"
-      port        = 8082
-      priority    = 105
+      # instance = "r6i.xlarge"
+      instance = "t3.medium"
+      port     = 8082
+      priority = 105
       policy_list = []
-      use_spot    = true  #  false means on demand instance
+      use_spot = true  #  false means on demand instance
+    }
+    jenkins = {
+      # instance = "r6i.xlarge"
+      instance = "t3.small"
+      port     = 8080
+      priority = 106
+      policy_list = []
+      use_spot = true
     }
   }
 }
